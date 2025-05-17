695ce4d4f57266e346e6c98719591be0a520e123
kimjy8937 <kimjy3520@gmail.com>
2025-05-18 01:22:23 +0900

    feat: CAT-98 피드 등록 관련 에러코드 추가

--- src/main/java/com/sixmycat/catchy/exception/ErrorCode.java
+++ src/main/java/com/sixmycat/catchy/exception/ErrorCode.java
@@ -16,7 +16,12 @@ public enum ErrorCode {
     EXPIRED_JWT("10004", "JWT 토큰이 만료되었습니다.", HttpStatus.UNAUTHORIZED),
     INVALID_JWT("10005", "잘못된 JWT 토큰입니다.", HttpStatus.UNAUTHORIZED),
     UNSUPPORTED_JWT("10006", "지원하지 않는 JWT 토큰입니다.", HttpStatus.UNAUTHORIZED),
-    EMPTY_JWT("10007", "JWT 클레임이 비어있습니다.", HttpStatus.UNAUTHORIZED);
+    EMPTY_JWT("10007", "JWT 클레임이 비어있습니다.", HttpStatus.UNAUTHORIZED),
+
+    // 피드
+    FEED_CONTENT_TOO_LONG("04001", "피드 내용은 500자 이하여야 합니다.", HttpStatus.BAD_REQUEST),
+    FEED_IMAGE_REQUIRED("04002", "피드에는 최소 1개의 이미지가 필요합니다.", HttpStatus.BAD_REQUEST),
+    FEED_IMAGE_TOO_MANY("04003", "피드에는 최대 5개의 이미지만 등록할 수 있습니다.", HttpStatus.BAD_REQUEST);
 
     private final String code;
     private final String message;
