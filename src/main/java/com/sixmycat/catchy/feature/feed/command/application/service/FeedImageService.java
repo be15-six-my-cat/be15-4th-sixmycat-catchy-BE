e71560e68fe47baac80f0903570e437ce719f529
kimjy8937 <kimjy3520@gmail.com>
2025-05-18 00:55:53 +0900

    feat: CAT-98 FeedImageService 클래스 작성

---
+++ src/main/java/com/sixmycat/catchy/feature/feed/command/application/service/FeedImageService.java
@@ -0,0 +1,23 @@
+package com.sixmycat.catchy.feature.feed.command.application.service;
+
+import com.sixmycat.catchy.common.s3.S3Uploader;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FeedImageService {
+
+    private final S3Uploader s3Uploader;
+
+    public String uploadSingleImage(MultipartFile file) {
+        return s3Uploader.uploadFile(file, "feed");
+    }
+
+    public List<String> uploadMultipleImages(List<MultipartFile> files) {
+        return s3Uploader.uploadFiles(files, "feed");
+    }
+}
