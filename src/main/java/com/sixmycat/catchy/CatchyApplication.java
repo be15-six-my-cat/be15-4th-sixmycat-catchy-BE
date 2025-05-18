a79050359b30c7f2464f8f6b5bb61d60168d5d85
PSY <develup-psy@github.com>
2025-05-18 14:40:44 +0900

    feat: S3 업로드 관련 설정 구축

--- src/main/java/com/sixmycat/catchy/CatchyApplication.java
+++ src/main/java/com/sixmycat/catchy/CatchyApplication.java
@@ -1,8 +1,11 @@
 package com.sixmycat.catchy;
 
+import com.sixmycat.catchy.common.s3.dto.S3Properties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(S3Properties.class)
 @SpringBootApplication
 public class CatchyApplication {
 
