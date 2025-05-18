8d22121c39be2c6d4dc710d8f9e9d950ec28c095
PSY <develup-psy@github.com>
2025-05-18 21:28:25 +0900

    feat: 컨트롤러 추가

--- src/main/java/com/sixmycat/catchy/feature/jjure/command/application/controller/JjureCommandController.java
+++ src/main/java/com/sixmycat/catchy/feature/jjure/command/application/controller/JjureCommandController.java
@@ -3,25 +3,42 @@ package com.sixmycat.catchy.feature.jjure.command.application.controller;
 import com.sixmycat.catchy.common.dto.ApiResponse;
 import com.sixmycat.catchy.common.s3.dto.PresignedUrlResponse;
 import com.sixmycat.catchy.common.s3.service.S3PresignedUrlService;
+import com.sixmycat.catchy.feature.jjure.command.application.dto.request.JjureUploadRequest;
+import com.sixmycat.catchy.feature.jjure.command.application.service.JjureService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/jjure/upload")
-@Tag(name = "", description = "")
+@Tag(name = "쭈르", description = "쭈르 업로드 API")
+@Slf4j
 public class JjureCommandController {
     private final S3PresignedUrlService presignedUrlService;
+    private final JjureService jjureService;
 
+    /* Presigned URL 생성 핸들러 */
     @GetMapping("/presigned-url")
     public ResponseEntity<ApiResponse<PresignedUrlResponse>> getPresignedUrl(@RequestParam String filename, @RequestParam String contentType) {
         PresignedUrlResponse response = presignedUrlService.generatePresignedUploadUrl(filename, contentType);
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
+    /* S3에 등록된 영상에 대한 메타데이터 저장 핸들러 */
+    @Operation(summary = "쭈르 업로드", description = "S3에 업로드된 쭈르 메타데이터를 저장합니다.")
+    @PostMapping
+    public ResponseEntity<ApiResponse<Void>> uploadJjure(
+            @Valid @RequestBody JjureUploadRequest request,
+            @AuthenticationPrincipal String memberId
+    ) {
+        log.info("userId: {}", memberId);
+        jjureService.uploadJjure(request, Long.parseLong(memberId));
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
 }
