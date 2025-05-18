608a3d0448a5970fdbf4fa5c894f13fa2a23e271
kimjy8937 <kimjy3520@gmail.com>
2025-05-19 00:55:55 +0900

    feat: CAT-91 피드 목록 조회 기능 구현

--- src/main/java/com/sixmycat/catchy/feature/feed/query/mapper/FeedQueryMapper.java
+++ src/main/java/com/sixmycat/catchy/feature/feed/query/mapper/FeedQueryMapper.java
@@ -21,4 +21,6 @@ public interface FeedQueryMapper {
     boolean isFeedLikedByUser(@Param("feedId") Long feedId, @Param("userId") Long userId);
 
     List<FeedSummaryResponse> findMyFeeds(Long memberId);
+
+    List<FeedBaseInfo> findFeedList();
 }
