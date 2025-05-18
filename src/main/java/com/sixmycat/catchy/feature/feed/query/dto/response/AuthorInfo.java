f96964f515d18f6892f8ca1ea2ccbe79a2e8abfe
kimjy8937 <kimjy3520@gmail.com>
2025-05-19 01:01:27 +0900

    test: CAT-91 피드 목록 조회 테스트 코드 추가

--- src/main/java/com/sixmycat/catchy/feature/feed/query/dto/response/AuthorInfo.java
+++ src/main/java/com/sixmycat/catchy/feature/feed/query/dto/response/AuthorInfo.java
@@ -2,11 +2,13 @@ package com.sixmycat.catchy.feature.feed.query.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@EqualsAndHashCode
 public class AuthorInfo {
     private Long authorId;
     private String nickname;
