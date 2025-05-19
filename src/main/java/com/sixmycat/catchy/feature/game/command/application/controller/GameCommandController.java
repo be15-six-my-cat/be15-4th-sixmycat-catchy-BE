df56486ac6fb594f1fe21e1d44cb270b2df73e07
PSY <develup-psy@github.com>
2025-05-19 22:52:01 +0900

    fix: 게임 점수 저장 api 변경

--- src/main/java/com/sixmycat/catchy/feature/game/command/application/controller/GameCommandController.java
+++ src/main/java/com/sixmycat/catchy/feature/game/command/application/controller/GameCommandController.java
@@ -9,7 +9,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/games")
+@RequestMapping("/api/v1/games/scores")
 @RequiredArgsConstructor
 public class GameCommandController {
 
