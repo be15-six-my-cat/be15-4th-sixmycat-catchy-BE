b51a4461df1d1ba2709b427ca8502965dc8d09f7
kimjy8937 <kimjy3520@gmail.com>
2025-05-19 18:11:08 +0900

    feat: CAT-106 쭈르 목록 조회 Service 인터페이스, 구현체 작성

--- src/main/java/com/sixmycat/catchy/feature/jjure/query/service/JjureQueryService.java
+++ src/main/java/com/sixmycat/catchy/feature/jjure/query/service/JjureQueryService.java
@@ -1,7 +1,11 @@
 package com.sixmycat.catchy.feature.jjure.query.service;
 
+import com.sixmycat.catchy.common.dto.PageResponse;
+import com.sixmycat.catchy.feature.feed.query.dto.response.FeedDetailResponse;
 import com.sixmycat.catchy.feature.jjure.query.dto.response.JjureDetailResponse;
 
 public interface JjureQueryService {
     JjureDetailResponse getJjureDetail(Long jjureId, Long userId);
+
+    PageResponse<JjureDetailResponse> getJjureList(Long userId, int page, int size);
 }
