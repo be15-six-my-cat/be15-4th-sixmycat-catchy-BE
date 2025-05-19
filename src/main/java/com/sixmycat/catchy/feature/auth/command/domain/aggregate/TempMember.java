f7c6bb25f3059ba1d79ba247deec12e03a197b08
didiha <hcring@naver.com>
2025-05-20 03:49:33 +0900

    feat: 소셜 계정에서 프로필 사진 가져오는 기능 삭제

--- src/main/java/com/sixmycat/catchy/feature/auth/command/domain/aggregate/TempMember.java
+++ src/main/java/com/sixmycat/catchy/feature/auth/command/domain/aggregate/TempMember.java
@@ -12,7 +12,6 @@ import java.io.Serializable;
 public class TempMember implements Serializable {
     private String email;
     private String social;
-    private String profileImage;
     private String name;
     private String contactNumber;
 }
