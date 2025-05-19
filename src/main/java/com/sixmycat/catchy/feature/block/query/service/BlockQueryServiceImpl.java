4f515ae8ad082ec8fda2e4bfa13957de436e4c95
ckaudgh <audgh3260@naver.com>
2025-05-19 11:58:47 +0900

    feat: CAT-83 차단 목록 조회 기능 수정

--- src/main/java/com/sixmycat/catchy/feature/block/query/service/BlockQueryServiceImpl.java
+++ src/main/java/com/sixmycat/catchy/feature/block/query/service/BlockQueryServiceImpl.java
@@ -1,5 +1,7 @@
 package com.sixmycat.catchy.feature.block.query.service;
 
+import com.sixmycat.catchy.exception.BusinessException;
+import com.sixmycat.catchy.exception.ErrorCode;
 import com.sixmycat.catchy.feature.block.query.dto.response.BlockResponse;
 import com.sixmycat.catchy.feature.block.query.mapper.BlockQueryRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +17,12 @@ public class BlockQueryServiceImpl implements BlockQueryService {
 
     @Override
     public List<BlockResponse> getBlockedUsers(Long blockerId) {
-        return blockQueryRepository.findBlockedUsers(blockerId);
+        List<BlockResponse> blockedUsers = blockQueryRepository.findBlockedUsers(blockerId);
+
+        if (blockedUsers == null || blockedUsers.isEmpty()) {
+            throw new BusinessException(ErrorCode.BLOCK_NOT_FOUND);
+        }
+
+        return blockedUsers;
     }
 }
