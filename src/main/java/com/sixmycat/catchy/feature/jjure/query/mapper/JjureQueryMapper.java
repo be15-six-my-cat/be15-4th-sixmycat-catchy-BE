92beeef0a148c3582b83680c57be9fa19d53658c
kimjy8937 <kimjy3520@gmail.com>
2025-05-19 17:36:57 +0900

    feat: CAT-110 쭈르 상세 조회 Query문 작성

--- src/main/java/com/sixmycat/catchy/feature/jjure/query/mapper/JjureQueryMapper.java
+++ src/main/java/com/sixmycat/catchy/feature/jjure/query/mapper/JjureQueryMapper.java
@@ -1,10 +1,13 @@
 package com.sixmycat.catchy.feature.jjure.query.mapper;
 
+import com.sixmycat.catchy.common.dto.CommentPreview;
+import com.sixmycat.catchy.feature.jjure.query.dto.response.JjureBaseInfo;
 import com.sixmycat.catchy.feature.jjure.query.dto.response.JjureCommentResponse;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 @Mapper
 public interface JjureQueryMapper {
@@ -12,4 +15,11 @@ public interface JjureQueryMapper {
     List<JjureCommentResponse> findCommentsByJjureId(@Param("jjureId") Long jjureId);
 
     boolean existsByJjureId(@Param("jjureId") Long jjureId);
+
+    Optional<JjureBaseInfo> findJjureBaseInfo(@Param("jjureId") Long jjureId);
+
+    Optional<CommentPreview> findLatestCommentPreview(@Param("jjureId") Long jjureId);
+
+    boolean isJjureLikedByUser(@Param("jjureId") Long jjureId, @Param("userId") Long userId);
+
 }
