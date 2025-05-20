5744a92fed8332cecd7ce1251003d2c3a94cb188
didiha <hcring@naver.com>
2025-05-20 10:57:46 +0900

    refactor: 중복되는 유효성 검사 부분 삭제

--- src/main/java/com/sixmycat/catchy/feature/member/query/controller/MemberQueryController.java
+++ src/main/java/com/sixmycat/catchy/feature/member/query/controller/MemberQueryController.java
@@ -1,17 +1,11 @@
 package com.sixmycat.catchy.feature.member.query.controller;
 
 import com.sixmycat.catchy.common.dto.ApiResponse;
-import com.sixmycat.catchy.exception.CustomJwtException;
-import com.sixmycat.catchy.exception.ErrorCode;
 import com.sixmycat.catchy.feature.member.query.dto.response.MemberResponse;
 import com.sixmycat.catchy.feature.member.query.service.MemberQueryService;
-import com.sixmycat.catchy.security.jwt.JwtTokenProvider;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.UnsupportedJwtException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.util.StringUtils;
-import io.jsonwebtoken.security.SignatureException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,31 +14,13 @@ import org.springframework.web.bind.annotation.*;
 public class MemberQueryController {
 
     private final MemberQueryService memberQueryService;
-    private final JwtTokenProvider jwtTokenProvider;
 
     @GetMapping("/me")
-    public ApiResponse<MemberResponse> getMyInfo(@RequestHeader("Authorization") String authHeader) {
-        if (!StringUtils.hasText(authHeader) || !authHeader.startsWith("Bearer ")) {
-            throw new CustomJwtException(ErrorCode.EMPTY_JWT);
-        }
+    public ApiResponse<MemberResponse> getMyInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Long memberId = Long.parseLong(authentication.getName());
 
-        String token = authHeader.substring(7);
-
-        try {
-            jwtTokenProvider.validateToken(token);
-        } catch (ExpiredJwtException e) {
-            throw new CustomJwtException(ErrorCode.EXPIRED_JWT);
-        } catch (UnsupportedJwtException e) {
-            throw new CustomJwtException(ErrorCode.UNSUPPORTED_JWT);
-        } catch (MalformedJwtException | SignatureException e) {
-            throw new CustomJwtException(ErrorCode.INVALID_JWT);
-        } catch (Exception e) {
-            throw new CustomJwtException(ErrorCode.UNAUTHORIZED_USER);
-        }
-
-        Long memberId = Long.parseLong(jwtTokenProvider.getUserIdFromJwt(token));
         MemberResponse member = memberQueryService.findById(memberId);
-
         return ApiResponse.success(member);
     }
 }
