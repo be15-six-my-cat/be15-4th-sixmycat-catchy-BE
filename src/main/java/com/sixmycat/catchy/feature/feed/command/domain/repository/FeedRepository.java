0c07b0b2f498f2212ec69502a2f9c50a3d423195
kimjy8937 <kimjy3520@gmail.com>
2025-05-20 21:06:35 +0900

    feat: CAT-206 findMemberIdById

--- src/main/java/com/sixmycat/catchy/feature/feed/command/domain/repository/FeedRepository.java
+++ src/main/java/com/sixmycat/catchy/feature/feed/command/domain/repository/FeedRepository.java
@@ -1,6 +1,8 @@
 package com.sixmycat.catchy.feature.feed.command.domain.repository;
 
 import com.sixmycat.catchy.feature.feed.command.domain.aggregate.Feed;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -8,4 +10,7 @@ public interface FeedRepository {
     Feed save(Feed feed);
 
     Optional<Feed> findById(Long feedId);
+
+    @Query("SELECT f.memberId FROM Feed f WHERE f.id = :feedId")
+    Long findMemberIdById(@Param("feedId") Long feedId);
 }
