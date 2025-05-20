acb75ee714b6ef87a5748e896371da2aa8c1b621
kimjy8937 <kimjy3520@gmail.com>
2025-05-20 16:03:45 +0900

    feat: CAT-199 좋아요한 피드 조회 서비스 작성

--- src/main/java/com/sixmycat/catchy/feature/feed/query/service/FeedQueryService.java
+++ src/main/java/com/sixmycat/catchy/feature/feed/query/service/FeedQueryService.java
@@ -8,4 +8,5 @@ public interface FeedQueryService {
     FeedDetailResponse getFeedDetail(Long feedId, Long userId);
     PageResponse<FeedSummaryResponse> getMyFeeds(Long memberId, int page, int size);
     PageResponse<FeedDetailResponse> getFeedList(Long userId, int page, int size);
+    PageResponse<FeedSummaryResponse> getLikedFeedList(Long memberId, int page, int size);
 }
