e02a72dcfd756616cb86204973e5b6611d4d04fe
PSY <develup-psy@github.com>
2025-05-22 16:20:14 +0900

    feat: 팔로우 신청 api 구현

---
+++ src/main/java/com/sixmycat/catchy/feature/follow/command/application/service/FollowService.java
@@ -0,0 +1,10 @@
+package com.sixmycat.catchy.feature.follow.command.application.service;
+
+public interface
+FollowService {
+    void follow(Long followerId, Long follwingId );
+//    void unfollow(Long followerId, Long followingId);
+//    void acceptFollowRequest(Long memberId, Long requesterId);
+//
+//    void rejectFollow(Long followingId, Long requesterId);
+}
